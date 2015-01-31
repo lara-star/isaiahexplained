lara-star/isaiahexplained

commit adfa673b3d31c8c4f31a014e0e66bd6fd990e279

Resolving issue #38

app/controllers/BibleController.php
@@ -87,7 +87,14 @@ class BibleController extends \BaseController {
             'book_title' => $title,
             'chapter_number' => $chapter_number,
             //'book_abbr' => $book_abbr,
-            'book_verses' => BibleRepository::GetChapterVerses($book_abbr, $chapter_number)
+            'book_verses' => BibleRepository::GetChapterVerses($book_abbr, $chapter_number),
+            'nav_links_light_left' => KJVPaginator::GetNav($book_abbr, $chapter_number, 'left', 'nav-links-light'),
+            'nav_links_light_right' => KJVPaginator::GetNav($book_abbr, $chapter_number, 'right', 'nav-links-light')
+        );
+
+        $header_data = array(
+            'heading_nav_left' => KJVPaginator::GetNav($book_abbr, $chapter_number, 'left', 'heading-chapters'),
+            'heading_nav_right' => KJVPaginator::GetNav($book_abbr, $chapter_number, 'right', 'heading-chapters')
         );
 
         View::share('book_abbr', $book_abbr);
@@ -95,8 +102,9 @@ class BibleController extends \BaseController {
         View::share('letters', WidgetRepository::GetConcordanceSelection());
 
         return View::make('layouts.master', $template_data)
-            ->nest('heading', 'headings.bible-chapter-index')
+            ->nest('heading', 'headings.bible-chapter')
             ->nest('top_nav', 'widgets.chapter-selection-kjv-top')
+            ->nest('heading', 'headings.chapter', $header_data)
             ->nest('mobile_search', 'widgets.search-iit-mobile')
             ->nest('content', 'bible.book-chapter', $content_data);
     }

public/views/headings/bible-chapter.mustache
@@ -1,0 +1,74 @@
+<div class="heading-chapters">
+
+    <div class="heading heading-full non-mobile">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="tabbable">
+                        <ul id="index-heading-tabs" class="nav nav-pills col-sm-8 col-md-9">
+                            <li><a href="/1#one_col" data-toggle="tab" title="Isaiah Institute Translation"><i class="fa fa-align-justify"></i><span>Isaiah Institute<br>Translation</span></a></li>
+                            <li><a href="/1#three_col" data-toggle="tab" title="Comparative Translation"><i class="fa fa-table"></i><span>Comparative<br>Translation</span></a></li>
+                            <li><a href="/1#commentary" data-toggle="tab" title="Apocalyptic Commentary"><i class="fa fa-quote-left"></i><span>Apocalyptic<br>Commentary</span></a></li>
+                            <li><a href="/1#concordance" data-toggle="tab" title="Interactive Concordance"><i class="fa fa-list"></i><span>Interactive<br>Concordance</span></a></li>
+                        </ul>
+                    </div>
+
+                    <!--<div class="nav-links col-sm-4 col-md-3">
+                        <ul class="nav">-->
+                    <!--<li>{{{heading_nav_left}}}</li>-->
+                    <!--<li><a id="nav-left-disabled" class="heading-nav-left fa fa-th hide-chapter-selection" href=""></a></li>-->
+                    <!--<li>{{{heading_nav_right}}}</li>-->
+                    <!--</ul>
+                </div>-->
+
+                    <button type="button" class="navbar-toggle pull-left" data-toggle="collapse" data-target=".navbar-collapse">
+                        <i class="fa fa-bars"></i>
+                    </button>
+
+                    <!--<h1><a class="fa fa-book" href="/"></a> <a href="#chaptermodal" class="modal-trigger" data-toggle="modal">Isaiah {{chapter_number}}</a></h1>-->
+
+                </div>
+            </div>
+        </div>
+    </div> <!-- End Heading Non-mobile -->
+
+    <div class="heading heading-mobile">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <button type="button" class="navbar-toggle pull-left" data-toggle="collapse" data-target=".navbar-collapse">
+                        <i class="fa fa-bars"></i>
+                    </button>
+                    <ul class="pull-right view-switcher">
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                                <i class="fa fa-ellipsis-v"></i>
+                            </a>
+                            <ul id="dropdown-heading-tabs" class="dropdown-menu dropdown-menu-right">
+                                <li><a href="/1#one_col" data-toggle="tab"><i class="fa fa-fw fa-align-justify"></i> Isaiah Institute Translation</a></li>
+                                <li><a href="/1#three_col" data-toggle="tab"><i class="fa fa-fw fa-table"></i> Comparative Translation</a></li>
+                                <li><a href="/1#commentary" data-toggle="tab"><i class="fa fa-fw fa-quote-left"></i> Apocalyptic Commentary</a></li>
+                                <li><a href="/1#concordance" data-toggle="tab"><i class="fa fa-fw fa-list"></i> Interactive Concordance</a></li>
+                                <li role="presentation" class="divider"></li>
+                                <li><a href="/"><i class="fa fa-fw fa-book"></i> Isaiah Chapter Index</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <div class="">
+                        <h1>The Holy Bible</h1>
+                        <p><a href="/bible/">King James Version</a></p>
+                    </div>
+
+                    <div class="nav-links">
+                        <ul class="nav">
+                            <li>{{{heading_nav_left}}}</li>
+                            <li><h1><a href="#chaptermodal" class="modal-trigger" data-toggle="modal">{{title}} {{chapter_number}}</a></h1></li>
+                            <li>{{{heading_nav_right}}}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- End Heading Mobile -->
+
+</div> <!-- End Heading Chapters -->

app/repositories/BibleRepository.php
@@ -48,6 +48,10 @@ class BibleRepository {
         return Book::where('book_lds_url', '=', $book_abbr)->first()->chapters;
     }
 
+    public static function GetBookChaptersCount($book_abbr) {
+        return Book::where('book_lds_url', '=', $book_abbr)->first()->chapters()->count();
+    }
+
     /**
      * Get Book Chapter Verses
      *

app/repositories/KJVPaginatorRepository.php
@@ -1,0 +1,95 @@
+<?php
+class KJVPaginator {
+
+    public static $limit = 0;
+
+    /**
+     * Gets previous/next navigation html for the specified chapter, direction, page area, and active tab
+     *
+     * @param string $book_abbr KJV book abbreviation
+     * @param int $chapter Current chapter
+     * @param string $direction Navigation direction
+     * @param string $page_area Area of page needing navigation
+     * @return string Navigation HTML
+     */
+    public static function GetNav($book_abbr, $chapter, $direction, $page_area) {
+        if($direction == 'left') {
+            --$chapter;
+        } else {
+            ++$chapter;
+        }
+
+        $nav_html = KJVPaginator::GetNavHtml($book_abbr, $chapter, $direction, $page_area);
+
+        return $nav_html;
+    }
+
+    /**
+     * Gets navigation html for the specified chapter, direction, page area, and active tab
+     *
+     * @param string $nav_book_abbr
+     * @param int $nav_chapter
+     * @param string $direction
+     * @param string $page_area
+     * @return string Navigation HTML
+     */
+    protected static function GetNavHtml($nav_book_abbr, $nav_chapter, $direction, $page_area) {
+        $nav_class = '';
+        $nav_area = '';
+
+        switch($page_area) {
+            case 'nav-links-light':
+                $nav_class .= 'btn btn-default ';
+                $nav_area = '-bottom';
+                break;
+            case 'heading-chapters':
+                $nav_area = '-top';
+                break;
+            default:
+                break;
+        }
+
+        $limit = KJVPaginator::$limit;
+        if($limit == 0) {
+            $limit = BibleRepository::GetBookChaptersCount($nav_book_abbr);
+            KJVPaginator::$limit = $limit;
+        }
+
+        if($nav_chapter >= 1 && $nav_chapter <= $limit) {
+            if($direction == 'left') {
+                $nav_class .= 'fa fa-angle-left';
+                if($page_area == 'heading-chapters') {
+                    $nav_class .= ' heading-nav-left';
+                }
+                $nav_id = 'nav-left' . $nav_area;
+            } else {
+                $nav_class .= 'fa fa-angle-right';
+                if($page_area == 'heading-chapters') {
+                    $nav_class .= ' heading-nav-right';
+                }
+                $nav_id = 'nav-right' . $nav_area;
+            }
+            $link = "/bible/{$nav_book_abbr}/{$nav_chapter}";
+        } else {
+            $link = '';
+            if($direction == 'left') {
+                if($page_area == 'heading-chapters') {
+                    $nav_class .= 'heading-nav-left ';
+                }
+                $nav_class .= 'fa fa-angle-left disabled';
+                $nav_id = 'nav-left-disabled';
+            } else {
+                if($page_area == 'heading-chapters') {
+                    $nav_class .= 'heading-nav-right ';
+                }
+                $nav_class .= 'fa fa-angle-right disabled';
+                $nav_id = 'nav-right-disabled';
+            }
+        }
+
+        $nav_html = "<a id=\"${nav_id}\" class=\"${nav_class}\" href=\"${link}\"></a>";
+
+        return $nav_html;
+    }
+
+}

public/views/bible/book-chapter.mustache
@@ -7,6 +7,8 @@
             <p id="verse_{{verse_number}}">{{verse_number}} {{scripture_text}}</p>
         {{/book_verses}}
     </div>
+    <hr>
+    <div class="bottom-links pull-right">{{> navigation.links-light}}</div>
 </main>
 <!--{\{> asides.chapter-index}}-->
 {{> asides.bible}}

commit bc5669417f310ba2899b51756208f363e8bec158

Fixing some regressions introduced in commit adfa673

app/controllers/BibleController.php
@@ -76,17 +76,15 @@ class BibleController extends \BaseController {
      */
     public function showBookChapter($book_abbr, $chapter_number)
     {
-        $title = BibleRepository::GetBookTitleFromAbbr($book_abbr);
+        $book_title = BibleRepository::GetBookTitleFromAbbr($book_abbr);
+
         $template_data = array(
-            'title' => "$title $chapter_number",
+            'title' => "{$book_title} {$chapter_number}",
             'body_id' => 'book-index',
             'body_css' => 'bible scriptures section-heading'
         );
 
         $content_data = array(
-            'book_title' => $title,
-            'chapter_number' => $chapter_number,
-            //'book_abbr' => $book_abbr,
             'book_verses' => BibleRepository::GetChapterVerses($book_abbr, $chapter_number),
             'nav_links_light_left' => KJVPaginator::GetNav($book_abbr, $chapter_number, 'left', 'nav-links-light'),
             'nav_links_light_right' => KJVPaginator::GetNav($book_abbr, $chapter_number, 'right', 'nav-links-light')
@@ -97,14 +95,17 @@ class BibleController extends \BaseController {
             'heading_nav_right' => KJVPaginator::GetNav($book_abbr, $chapter_number, 'right', 'heading-chapters')
         );
 
+        View::share('book_title', $book_title);
+        View::share('chapter_number', $chapter_number);
         View::share('book_abbr', $book_abbr);
         View::share('chapters', WidgetRepository::GetKJVChapterSelection($chapter_number, count(BibleRepository::GetBookChapters($book_abbr))));
         View::share('letters', WidgetRepository::GetConcordanceSelection());
 
         return View::make('layouts.master', $template_data)
-            ->nest('heading', 'headings.bible-chapter')
-            ->nest('top_nav', 'widgets.chapter-selection-kjv-top')
-            ->nest('heading', 'headings.chapter', $header_data)
+            //->nest('heading', 'headings.bible-chapter')
+            ->nest('top_nav', 'widgets.chapter-selection-top-kjv-book-chapter')
+            ->nest('chapter_modal', 'modals.bible-chapter')
+            ->nest('heading', 'headings.bible-chapter', $header_data)
             ->nest('mobile_search', 'widgets.search-iit-mobile')
             ->nest('content', 'bible.book-chapter', $content_data);
     }

public/views/headings/bible-chapter-index.mustache
@@ -61,9 +61,9 @@
 
                     <!--<div class="nav-links">
                         <ul class="nav">
-                            <li>{{{heading_nav_left}}}</li>
-                            <li><h1><a href="#chaptermodal" class="modal-trigger" data-toggle="modal">Isaiah {{chapter_number}}</a></h1></li>
-                            <li>{{{heading_nav_right}}}</li>
+                            <li>{\{{heading_nav_left}}}</li>
+                            <li><h1><a href="#chaptermodal" class="modal-trigger" data-toggle="modal">Isaiah {\{chapter_number}}</a></h1></li>
+                            <li>{\{{heading_nav_right}}}</li>
                         </ul>
                     </div>-->
                 </div>

public/views/headings/bible-chapter.mustache
@@ -13,19 +13,19 @@
                         </ul>
                     </div>
 
-                    <!--<div class="nav-links col-sm-4 col-md-3">
-                        <ul class="nav">-->
-                    <!--<li>{{{heading_nav_left}}}</li>-->
-                    <!--<li><a id="nav-left-disabled" class="heading-nav-left fa fa-th hide-chapter-selection" href=""></a></li>-->
-                    <!--<li>{{{heading_nav_right}}}</li>-->
-                    <!--</ul>
-                </div>-->
+                    <div class="nav-links col-sm-4 col-md-3">
+                        <ul class="nav">
+                            <li>{{{heading_nav_left}}}</li>
+                            <li><a id="nav-left-disabled" class="heading-nav-left fa fa-th hide-chapter-selection" href=""></a></li>
+                            <li>{{{heading_nav_right}}}</li>
+                        </ul>
+                    </div>
 
                     <button type="button" class="navbar-toggle pull-left" data-toggle="collapse" data-target=".navbar-collapse">
                         <i class="fa fa-bars"></i>
                     </button>
 
-                    <!--<h1><a class="fa fa-book" href="/"></a> <a href="#chaptermodal" class="modal-trigger" data-toggle="modal">Isaiah {{chapter_number}}</a></h1>-->
+                    <!--<h1><a class="fa fa-book" href="/"></a> <a href="#chaptermodal" class="modal-trigger" data-toggle="modal">Isaiah {\{chapter_number}}</a></h1>-->
 
                 </div>
             </div>
@@ -39,6 +39,7 @@
                     <button type="button" class="navbar-toggle pull-left" data-toggle="collapse" data-target=".navbar-collapse">
                         <i class="fa fa-bars"></i>
                     </button>
+
                     <ul class="pull-right view-switcher">
                         <li class="dropdown">
                             <a class="dropdown-toggle" data-toggle="dropdown" href="#">
@@ -54,15 +55,11 @@
                             </ul>
                         </li>
                     </ul>
-                    <div class="">
-                        <h1>The Holy Bible</h1>
-                        <p><a href="/bible/">King James Version</a></p>
-                    </div>
 
                     <div class="nav-links">
                         <ul class="nav">
                             <li>{{{heading_nav_left}}}</li>
-                            <li><h1><a href="#chaptermodal" class="modal-trigger" data-toggle="modal">{{title}} {{chapter_number}}</a></h1></li>
+                            <li><h1><a href="#chaptermodal" class="modal-trigger" data-toggle="modal">{{book_title}} {{chapter_number}}</a></h1></li>
                             <li>{{{heading_nav_right}}}</li>
                         </ul>
                     </div>

public/views/modals/bible-chapter.mustache
@@ -1,0 +1,13 @@
+<div id="chaptermodal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+            <div class="modal-header">
+                <a class="modal-close pull-right" data-dismiss="modal" aria-hidden="true"><i class="fa fa-times"></i></a>
+                <h4 class="modal-title" id="myModalLabel">{{title}} Chapter Index</h4>
+            </div>
+            <div class="modal-body">
+                {{> widgets.chapter-selection-kjv-book-chapter}}
+            </div>
+        </div>
+    </div>
+</div>

public/views/tabs/commentary.mustache
@@ -25,4 +25,5 @@
     {{> widgets.ad-commentary}}
 </main>
 
-{{> asides.commentary}}
+<!--{\{> asides.commentary}}-->
+{{> asides.isaiah}}

public/views/widgets/chapter-selection-kjv-book-chapter.mustache
@@ -1,0 +1,5 @@
+<ul class="chapter-selection">
+    {{#chapters}}
+        <li><a class="btn {{button_class}} btn-chapter-sel" href="/bible/{{book_abbr}}/{{selection_number}}">{{selection_number}}</a></li>
+    {{/chapters}}
+</ul>

public/views/widgets/chapter-selection-top-kjv-book-index.mustache
@@ -1,0 +1,7 @@
+<div class="row bible-chapters">
+    <ul class="chapter-selection cs-top">
+        {{#chapters}}
+            <li><a class="btn {{button_class}} btn-chapter-sel" href="/bible/{{book_abbr}}/{{selection_number}}">{{selection_number}}</a></li>
+        {{/chapters}}
+    </ul>
+</div>